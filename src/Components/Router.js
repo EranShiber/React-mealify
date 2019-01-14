04e937e6b4a5b1f080699b62a31e011694270250
eran <eranshiber7@gmail.com>
2019-01-14 13:51:34 +0200

    changes routes

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import App from '../App'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter , Route, Switch } from 'react-router-dom';
 import Recipe from './Recipe';
 import LikedList from './LikedList';
 
 class Router extends React.Component{
     render(){
     return(
-        <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
+        <HashRouter>
         <Switch >
             <Route path="/" component={App} exact />
             <Route path="/recipe/:id" component={Recipe} />
             <Route path="/likes" component={LikedList} exact />
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
     )
 }
 }
