806a8cd4d9b8dec16bfa4851087755dbf27ee435
eran <eranshiber7@gmail.com>
2019-01-14 15:46:43 +0200

    Updates

---
+++ precache-manifest.1ee156728a6a040902e65f75c21ff937.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "607dab2918796bb3fa2f",
+    "url": "/React-mealify/static/js/runtime~main.607dab29.js"
+  },
+  {
+    "revision": "5cb506dd799f6554d97c",
+    "url": "/React-mealify/static/js/main.5cb506dd.chunk.js"
+  },
+  {
+    "revision": "26bc8a2df8286c7c65c3",
+    "url": "/React-mealify/static/js/1.26bc8a2d.chunk.js"
+  },
+  {
+    "revision": "5cb506dd799f6554d97c",
+    "url": "/React-mealify/static/css/main.29cb25c2.chunk.css"
+  },
+  {
+    "revision": "4b2300fc19e97d640eade25a8245ec45",
+    "url": "/React-mealify/index.html"
+  }
+];
